a000ffc85f4767fee14b3926d6abf4f952882447
Simon Cooper <cooper.sf@gmail.com>
2019-01-16 20:55:25 +0000

    Update connection.rb
    Corrected spelling - verfied > verified

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,12 +3,12 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_verfied_user
+      self.current_user = find_verified_user
     end
 
     protected
 
-    def find_verfied_user
+    def find_verified_user
       if current_user = env['warden'].user
         current_user
       else
